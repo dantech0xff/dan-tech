eb62f1c9dab02d4d1244e793c0fcfdd79ec426f3
danhtran-dev <danhtran.dev@outlook.com>
2022-12-20 22:09:11 +0700

    add images

--- pages/_app.js
+++ pages/_app.js
@@ -11,17 +11,17 @@ import siteMetadata from "../data/siteMetadata";
 NProgress.configure({ showSpinner: false });
 
 Router.onRouteChangeStart = () => {
-  // console.log('onRouteChangeStart triggered');
+  console.log("onRouteChangeStart triggered");
   NProgress.start();
 };
 
 Router.onRouteChangeComplete = () => {
-  // console.log('onRouteChangeComplete triggered');
+  console.log("onRouteChangeComplete triggered");
   NProgress.done();
 };
 
 Router.onRouteChangeError = () => {
-  // console.log('onRouteChangeError triggered');
+  console.log("onRouteChangeError triggered");
   NProgress.done();
 };
 
